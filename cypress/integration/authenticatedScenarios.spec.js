7ddee8b5290fb04828c428fbee4193e97274f952
gustavosmdt <gustavoschmidt147@gmail.com>
2021-12-15 21:19:49 -0300

    Add missing docs

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress"/>
+/// <reference path="../support/commands.d.ts" />
 
 describe('Scenarios where authentication is a pre-requirement', () => {
     beforeEach(() => {
